21ac6660e25bb7515ceb0833893953efba6565b1
juliadavydenko <julietta12345@yahoo.com>
2023-05-20 17:27:30 +0200

    Added routs to meals and reservations

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
+
 const path = require("path");
 
 const mealsRouter = require("./api/meals");
+const reservationsRouter = require("./api/reservations");
 const buildPath = path.join(__dirname, "../../dist");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
@@ -21,6 +23,7 @@ app.use(express.json());
 app.use(cors());
 
 router.use("/meals", mealsRouter);
+router.use("/reservations", reservationsRouter);
 //Future meals route 
 app.get("/future-meals", (req, res) => {
   knex
